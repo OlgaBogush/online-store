43a9025af0465a7d793e3d500ea05cbbe62e51b8
OlgaBogush <bohush.olga@gmail.com>
2025-08-01 02:10:51 +0300

    feat: add Sidebar component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,20 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Route, Routes } from "react-router-dom"
+import { useDispatch } from "react-redux"
 
 import Home from "../Home/Home"
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
 import Sidebar from "../Sidebar/Sidebar"
+import { getCategories } from "../../features/categories/categoriesSlice"
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getCategories())
+  }, [dispatch])
+
   return (
     <div className="app">
       <Header />
@@ -17,7 +25,7 @@ const App = () => {
           <Route index element={<Home />} />
         </Routes>
       </div>
-      
+
       <Footer />
     </div>
   )
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from "react"
+import { NavLink } from "react-router-dom"
+import { useSelector } from "react-redux"
+
+import styles from "../../styles/Sidebar.module.css"
 
 const Sidebar = () => {
+  const { list } = useSelector(state => state.categories)
+  console.log(list);
+
   return (
-    <div>
-      Sidebar
-    </div>
+    <section className={styles.sidebar}>
+      <div className={styles.title}>CATEGORIES</div>
+      <nav>
+        <ul className={styles.menu}>
+          {list.map(({id, name}) => (
+              <li key={id}>
+                <NavLink className={({isActive}) => `${styles.link} ${isActive ? styles.active : ""}`} to={`/categories/${id}`}>{name}</NavLink>
+              </li>
+            ))}
+        </ul>
+      </nav>
+      <div className={styles.footer}>
+        <a href="/help" className={styles.link} target="_blank">
+          Help
+        </a>
+        <a
+          href="/terms"
+          className={styles.link}
+          target="_blank"
+          style={{ textDecoration: "underline" }}
+        >
+          Terms & Conditions
+        </a>
+      </div>
+    </section>
   )
 }
 
