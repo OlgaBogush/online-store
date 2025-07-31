ad49bf1ba8dde7fda66d21858ba5a021298b05fa
OlgaBogush <bohush.olga@gmail.com>
2025-07-31 22:39:49 +0300

    feat: add Header component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React from "react"
+import { Route, Routes } from "react-router-dom"
 
-import Home from '../Home/Home'
+import Home from "../Home/Home"
+import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
+import Sidebar from "../Sidebar/Sidebar"
 
 const App = () => {
   return (
-    <div className='app'>
-      <Routes>
-        <Route index element={<Home />} />
-      </Routes>
+    <div className="app">
+      <Header />
+
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </div>
+      
+      <Footer />
     </div>
   )
 }
