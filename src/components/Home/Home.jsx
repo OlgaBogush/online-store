77f82d9ff42b43e17133945982bc21a3bcd3285f
OlgaBogush <bohush.olga@gmail.com>
2025-08-01 15:15:13 +0300

    feat: add Products component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react"
+import { useSelector } from "react-redux"
 
 import Poster from "../Poster/Poster"
+import Products from "../Products/Products"
 
 const Home = () => {
+  const {list} = useSelector(state => state.products)
+
   return (
     <>
       <Poster />
+      <Products title="Trending" products={list} amount={5} />
     </>
   )
 }
