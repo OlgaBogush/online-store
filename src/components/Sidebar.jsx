3e1f6fe905c853f6c6c2086f52bace04ddda3875
OlgaBogush <bohush.olga@gmail.com>
2026-02-05 13:35:14 +0300

    feat: add Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,23 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+
+import styles from "../styles/Sidebar.module.css"
 
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  return (
+    <section className={styles.sidebar}>
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink className={styles.link}>some link</NavLink>
+          </li>
+          <li>
+            <NavLink className={styles.link}>some link</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  )
 }
 
 export default Sidebar
