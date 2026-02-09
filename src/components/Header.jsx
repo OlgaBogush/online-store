4d0d497ca508deffd01d847389a70642a49d274d
OlgaBogush <bohush.olga@gmail.com>
2026-02-09 17:32:13 +0300

    fix: correct svg icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 import logo from "../images/handbag.png"
-import avatar from "../images/avatar.png"
 import styles from "../styles/Header.module.css"
 
 const Header = () => {
@@ -15,16 +14,23 @@ const Header = () => {
 
       <div className={styles.info}>
         <div className={styles.user}>
-          <div
+          <svg
             className={styles.avatar}
-            style={{ backgroundImage: `url(${avatar})` }}
-          />
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+          >
+            <path d="M64 160c0-53 43-96 96-96s96 43 96 96c0 42.7-27.9 78.9-66.5 91.4-28.4 9.2-61.5 35.3-61.5 76.6l0 24c0 17.7 14.3 32 32 32s32-14.3 32-32l0-24c0-1.7 .6-4.1 3.5-7.3 3-3.3 7.9-6.5 13.7-8.4 64.3-20.7 110.8-81 110.8-152.3 0-88.4-71.6-160-160-160S0 71.6 0 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm96 352c22.1 0 40-17.9 40-40s-17.9-40-40-40-40 17.9-40 40 17.9 40 40 40z" />
+          </svg>
           <div className={styles.username}>Guest</div>
         </div>
 
         <form className={styles.form}>
-          <svg className={styles.icon}>
-            <use xlinkHref="/sprite.svg#search" />
+          <svg
+            className={styles.search}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376C296.3 401.1 253.9 416 208 416 93.1 416 0 322.9 0 208S93.1 0 208 0 416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
           </svg>
 
           <div className={styles.input}>
@@ -41,14 +47,22 @@ const Header = () => {
           {true && <div className={styles.box}></div>}
         </form>
         <div className={styles.account}>
-          <Link to="/" className={styles.favourites}>
-            <svg className={styles["icon-fav"]}>
-              <use xlinkHref="/sprite.svg#heart" />
+          <Link to="/" className={styles.heart}>
+            <svg
+              className={styles["icon-heart"]}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path d="M378.9 80c-27.3 0-53 13.1-69 35.2l-34.4 47.6c-4.5 6.2-11.7 9.9-19.4 9.9s-14.9-3.7-19.4-9.9l-34.4-47.6c-16-22.1-41.7-35.2-69-35.2-47 0-85.1 38.1-85.1 85.1 0 49.9 32 98.4 68.1 142.3 41.1 50 91.4 94 125.9 120.3 3.2 2.4 7.9 4.2 14 4.2s10.8-1.8 14-4.2c34.5-26.3 84.8-70.4 125.9-120.3 36.2-43.9 68.1-92.4 68.1-142.3 0-47-38.1-85.1-85.1-85.1zM271 87.1c25-34.6 65.2-55.1 107.9-55.1 73.5 0 133.1 59.6 133.1 133.1 0 68.6-42.9 128.9-79.1 172.8-44.1 53.6-97.3 100.1-133.8 127.9-12.3 9.4-27.5 14.1-43.1 14.1s-30.8-4.7-43.1-14.1C176.4 438 123.2 391.5 79.1 338 42.9 294.1 0 233.7 0 165.1 0 91.6 59.6 32 133.1 32 175.8 32 216 52.5 241 87.1l15 20.7 15-20.7z" />
             </svg>
           </Link>
-          <Link to="/cart" className={styles.bag}>
-            <svg className={styles["icon-bag"]}>
-              <use xlinkHref="/sprite.svg#bag" />
+          <Link to="/cart" className={styles.cart}>
+            <svg
+              className={styles["icon-cart"]}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 512"
+            >
+              <path d="M24-16C10.7-16 0-5.3 0 8S10.7 32 24 32l45.3 0c3.9 0 7.2 2.8 7.9 6.6l52.1 286.3c6.2 34.2 36 59.1 70.8 59.1L456 384c13.3 0 24-10.7 24-24s-10.7-24-24-24l-255.9 0c-11.6 0-21.5-8.3-23.6-19.7l-5.1-28.3 303.6 0c30.8 0 57.2-21.9 62.9-52.2L568.9 69.9C572.6 50.2 557.5 32 537.4 32l-412.7 0-.4-2c-4.8-26.6-28-46-55.1-46L24-16zM208 512a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm224 0a48 48 0 1 0 0-96 48 48 0 1 0 0 96z" />
             </svg>
             <span className={styles.count}>2</span>
           </Link>
